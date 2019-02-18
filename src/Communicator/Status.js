23363ecb8a12952e3a4217eac71cdb2d062747dd
Kysune <szymonlisowiec@gmail.com>
2019-02-18 19:28:42 +0100

    Hotfix for Communicator\Status

--- src/Communicator/Status.js
+++ src/Communicator/Status.js
@@ -48,7 +48,7 @@ class Status extends LauncherStatus {
       notAcceptingReason: party.notAcceptingReason,
 
       source: {
-        accountAd: party.sourceId,
+        accountId: party.sourceId,
         displayName: party.sourceDisplayName,
         platform: party.sourcePlatform,
       },
