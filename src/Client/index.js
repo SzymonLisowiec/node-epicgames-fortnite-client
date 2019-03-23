be485a33e89fca34305c356ef74495f6d8119d4c
Top Dog <34892671+mytopdog@users.noreply.github.com>
2019-03-23 17:04:05 +0000

    Fixed vbucks property.

--- src/Client/index.js
+++ src/Client/index.js
@@ -311,6 +311,10 @@ class Client extends Events {
         case 'Currency:MtxGiveaway':
           sum += currency.quantity;
           break;
+          
+        case 'Currency:MtxPurchased':
+          sum += currency.quantity;
+          break;
         
         default:
           this.launcher.debug.print(`Unknown currency with template '${currency.templateId}'`);
